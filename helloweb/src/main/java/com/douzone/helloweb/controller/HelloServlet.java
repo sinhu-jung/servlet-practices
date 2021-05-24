8446ffeeb9c8c7b19a44b889ef0f5ae052a36bd7
sinhu-jung <tlsgn26@naver.com>
2021-05-24 13:30:59 +0900

    ...

--- helloweb/src/main/java/com/douzone/helloweb/controller/HelloServlet.java
+++ helloweb/src/main/java/com/douzone/helloweb/controller/HelloServlet.java
@@ -13,8 +13,9 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
 		PrintWriter pw = response.getWriter();
-		pw.print("<h1>Hello World</h1>");
+		pw.print("<h1>안녕</h1>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
