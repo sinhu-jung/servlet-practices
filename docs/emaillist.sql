b1c2145859e3bc16443f06f18d336c8ca81f2e49
sinhu-jung <tlsgn26@naver.com>
2021-05-25 15:50:30 +0900

    ....

--- docs/emaillist.sql
+++ docs/emaillist.sql
@@ -1,1 +1,9 @@
+-- schema
 desc emaillist;
+
+-- list
+select no, first_name, last_name, email from emaillist order by no desc;
+
+-- add
+insert into emaillist values (null, '둘', '리', 'dooly@gmail.com');
+insert into emaillist values (null, '마', '이콜', 'michol@gmail.com');
