83f918d7d3c22b10c340a9dc1523f765486b08dc
sinhu-jung <tlsgn26@naver.com>
2021-05-26 10:50:54 +0900

    emaillist02 #1

---
+++ emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -0,0 +1,45 @@
+package com.douzone.emaillist.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
+
+public class EmaillistController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//get과 post 방식을 한 곳에서 같이 처리
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// post 방식으로 전달받는 파라미터 값의 엔코딩 처리
+		request.setCharacterEncoding("utf-8"); //body의 데이터들의 encoding을 utf-8로 하겠다.
+		
+		String action = request.getParameter("a");
+		
+		if("form".equals(action)) {
+		} else if("add".equals(action)) {
+		} else {
+			/* defalult request(action) */
+			// 1. 요청 처리
+			List<EmaillistVo> list = new EmaillistDao().findAll();
+			
+			// 2. request 범위에 데이터(객체) 저장
+			request.setAttribute("list", list);
+			
+			// 3. view로 포워딩
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
